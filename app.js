25bd7f55499856b9e7cf9deebbc0d72ba3dba5c1
njerumtwaiti <bernardnjerumtwaiti@gmail.com>
2022-04-26 10:22:29 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const computerchoiceDispaly = document.getElementById('computer-choice')
+const userrchoiceDisplay = document.getElementById('user-choice')
+const resultdisplay = document.getElementById('result')
+
+const possiblechoice = document.querySelectorAll('button')
+let userrchoice
+let computerChoice
+let result
+possiblechoice.forEach( possiblechoice => possiblechoice.addEventListener('click', (e) =>{
+
+    userrchoice = e.target.id
+    userrchoiceDisplay.innerHTML = userrchoice
+    generateComputerChoice()
+    getResult()
+}) )
+
+function generateComputerChoice(){
+    const randomNumber = Math.floor( Math.random() * 3 ) +1
+    
+    if(randomNumber == 1){
+        computerChoice = "rock"
+    }
+    if(randomNumber == 3){
+        computerChoice = "paper"
+    }
+    if(randomNumber == 2){
+        computerChoice = "scissor"
+    }
+
+    computerchoiceDispaly.innerHTML = computerChoice
+}
+
+function getResult(){
+
+    if(computerChoice == userrchoice){
+        result = "its a draw "
+    }
+    if(computerChoice === "rock" && userrchoice ==="scissor"){
+        result = "you lose "
+    }
+    if(computerChoice === "rock" && userrchoice ==="rock"){
+        result = "you win "
+    }
+    if(computerChoice === "rock" && userrchoice ==="paper"){
+        result = "you lose "
+    }
+    if(computerChoice === "paper" && userrchoice ==="scissor"){
+        result = "you lose "
+    }
+    if(computerChoice === "paper" && userrchoice ==="rock"){
+        result = "you lose "
+    }
+    if(computerChoice === "paper" && userrchoice ==="paper"){
+        result = "you win "
+    }
+    if(computerChoice === "scissor" && userrchoice ==="scissor"){
+        result = "you win "
+    }
+    if(computerChoice === "scissor" && userrchoice ==="rock"){
+        result = "you lose "
+    }
+    if(computerChoice === "scissor" && userrchoice ==="paper"){
+        result = "you lose "
+    }
+
+    resultdisplay.innerHTML = result
+}
